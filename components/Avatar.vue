32db3e296de761af211a7546c7822b713da20c5f
VietManh4112 <danicetea@gmail.com>
2024-03-26 12:46:13 +0700

    `fix`: _id.vue

--- components/Avatar.vue
+++ components/Avatar.vue
@@ -1,23 +1,25 @@
 <template>
-    <nuxt-link :to="'/ethnic/' + role">
-      <svg preserveAspectRatio='none' data-bbox='-4 -10 160 160' viewBox='-4 -10 160 160' height='160' width='160'
-          xmlns='http://www.w3.org/2000/svg' data-type='shape'>
-          <defs>
-            <clipPath id="circleClip">
-              <circle cx="76" cy="70" r="80" />
-            </clipPath>
-          </defs>
-          <rect x="-4" y="-10" width="160" height="160" fill="rgba(249,220,200,255)" clip-path="url(#circleClip)" />
-          <g clip-path="url(#circleClip)">
-            <image
-              :xlink:href="imageUrl" width="151" height="148" />
-              
-            <foreignObject x="16" y="98" width="160" height="30">
-              <div xmlns="http://www.w3.org/1999/xhtml" class="image-caption">{{ role }}</div>
-            </foreignObject>
-          </g>
-      </svg>
-    </nuxt-link>
+    <div @click="sendId">
+      <nuxt-link :to="'/ethnic/' + role">
+        <svg preserveAspectRatio='none' data-bbox='-4 -10 160 160' viewBox='-4 -10 160 160' height='160' width='160'
+            xmlns='http://www.w3.org/2000/svg' data-type='shape'>
+            <defs>
+              <clipPath id="circleClip">
+                <circle cx="76" cy="70" r="80" />
+              </clipPath>
+            </defs>
+            <rect x="-4" y="-10" width="160" height="160" fill="rgba(249,220,200,255)" clip-path="url(#circleClip)" />
+            <g clip-path="url(#circleClip)">
+              <image
+                :xlink:href="imageUrl" width="151" height="148" />
+                
+              <foreignObject x="16" y="98" width="160" height="30">
+                <div xmlns="http://www.w3.org/1999/xhtml" class="image-caption">{{ role }}</div>
+              </foreignObject>
+            </g>
+        </svg>
+      </nuxt-link>
+    </div>
 </template>
 
 <script>
@@ -26,6 +28,12 @@
         props: {
             imageUrl: String,
             role: String,
+        },
+
+        methods: {
+          sendId() {
+            console.log(this.role);
+          }
         }
     }
 </script>
---
+++ pages/ethnic/_id.vue
@@ -0,0 +1,29 @@
+<template>
+    <CostumeInfomation :imgleft="require(`@/assets/img/${productId}1.png`)" :imgright="require(`@/assets/img/${productId}2.png`)" :index="index"></CostumeInfomation>
+</template>
+
+<script>
+import CostumeInfomation from '@/components/CostumeInfomation.vue'
+export default {
+
+    data() {
+        return {
+            index: '',
+        }
+    },
+
+    components: {
+        CostumeInfomation,
+    },
+
+    computed: {
+        productId() {
+            const id = this.$route.params.id.toLowerCase();
+            const strippedId = id.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+            const cleanedId = strippedId.replace(/[-`]/g, "");
+            const trimmedId = cleanedId.replace(/\s+/g, "");
+            return trimmedId;
+        }
+    },
+}
+</script>
