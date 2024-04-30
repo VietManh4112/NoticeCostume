bff7fb7c1a10e936cbab577ac834e1bd40afc91d
VietManh4112 <danicetea@gmail.com>
2024-04-30 16:27:56 +0700

    `test`

--- helper/resource.js
+++ helper/resource.js
@@ -25,7 +25,7 @@ const Resource = {
     },
     mainText: {
         vi:'"54 dân tộc anh em" là cụm từ có lẽ khá quen thuộc với người Việt Nam. Nhưng liệu bạn có biết rõ những người anh em của chúng ta trông thế nào, ăn món gì, sống ra sao? Chúng tôi sẽ khắc họa lại những nét đặc trưng của trang phục các dân tộc, cho bạn đọc một cái nhìn sơ lược về diện mạo của những người anh em tưởng xa mà lại rất gần.',
-        en:'"54 ethnic groups" is a phrase that is probably quite familiar to Vietnamese people. But do you know what our brothers look like, what they eat, and how they live? We will portray the characteristics of ethnic costumes, giving readers a glimpse of the appearance of brothers who seem far away but are very close.'
+        en:'"54 ethnic groups" is a phrase that is probably quite familiar to Vietnamese people. But do you know clearly what our brothers look like, what they eat, and how they live? We will portray the characteristics of ethnic costumes, giving readers a glimpse of the appearance of brothers who seem far away but are actually very close.'
     },
     text1: {
         vi: 'Tên tự gọi:',
--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,6 @@ export default {
   router: {
     base: '/NoticeCostume/',
   },
-  buildDir: 'nuxt-dist',
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
@@ -86,6 +85,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    publicPath: '/_nuxt/',
+    publicPath: '/assets/',
   },
 }
