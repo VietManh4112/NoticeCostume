bd7445bb365a623ef70d2a7bc1bad61d3c65ec04
VietManh4112 <danicetea@gmail.com>
2024-05-19 10:30:43 +0700

    `style`: vee-validate

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['~/plugins/vee-validate.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -106,5 +106,8 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     publicPath: '/assets/',
+    transpile: [
+      'vee-validate/dist/rules'
+    ]
   },
 }
